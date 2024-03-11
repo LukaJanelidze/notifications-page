33ebdbd2ff2c9ea3161d0da3bfd4fa1e03011128
Luka Janelidze <lukajanelidze5@gmail.com>
2024-03-11 23:17:56 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Notification from './Components/Notification'
+
+function App() {
+
+  return (
+    <main className='bg-[#f7fafd] h-[100vh] flex justify-center items-center font-jakarta'>
+      <Notification />
+    </main>
+  )
+}
+
+export default App
---
+++ src/Components/Notification.jsx
@@ -0,0 +1,11 @@
+export default function FirstSection() {
+    return (
+        <div className="bg-white w-[730px] pl-[32px] pt-[33px] pr-[30px] pb-[18px] rounded-[15px]">
+            
+            <header className="flex justify-between items-center">
+                <h1 className="text-[24px] font-extrabold h-[30px] m-0a">Notifications <span className="text-[16px] text-white bg-[#0a327b] font-extrabold pb-[4px] pt-[1px] px-[11px] rounded-[6px] ml-[11px]">3</span></h1>
+                <p>Mark all as read</p>
+            </header>
+        </div>
+    )
+}
